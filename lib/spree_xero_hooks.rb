527ce1da93cf24b75bb4fe38e484a7b7a963e143
Ian Viney <ian@viney.net.nz>
2011-12-13 20:34:09 +1300

    Make hooks compatible with Spree 0.50.4.

--- lib/spree_xero_hooks.rb
+++ lib/spree_xero_hooks.rb
@@ -1,11 +1,21 @@
 class SpreeXeroHooks < Spree::ThemeSupport::HookListener
-  Deface::Override.new(:virtual_path => "admin/configurations/index",
-                       :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                       :text => "
-        <tr>
-          <td><%= link_to t('xero_settings'), admin_xero_settings_path %></td>
-          <td><%= t(\"xero_settings\") %></td>
-        </tr>
-      ",
-                       :disabled => false)
+  #Deface::Override.new(:virtual_path => "admin/configurations/index",
+  #                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+  #                     :text => "
+  #      <tr>
+  #        <td><%= link_to t('xero_settings'), admin_xero_settings_path %></td>
+  #        <td><%= t(\"xero_settings\") %></td>
+  #      </tr>
+  #    ",
+  #                     :disabled => false)
+
+  insert_after :admin_configurations_menu do
+  <<HTML
+  <tr>
+    <td><%= link_to t('xero_settings'), admin_xero_settings_path %></td>
+    <td><%= t(\"xero_settings\") %></td>
+  </tr>
+HTML
+  end
+
 end
